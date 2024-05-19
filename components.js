e685cfb547ce3a42c15ff8d99a29831839c3f0fc
xbaubes <xavierbaubes@gmail.com>
2024-05-19 13:12:41 +0200

    github link repository

--- components.js
+++ components.js
@@ -1,3 +1,12 @@
+let modules = [
+    ["aplicacions","https://github.com/xbaubes/AplicacionsInformatiques"],
+    ["bdd","https://github.com/xbaubes/BasesDeDades"],
+    ["web","https://github.com/xbaubes/DesenvolupamentWeb"],
+    ["programacio","https://github.com/xbaubes/Programacio"],
+    ["so","https://github.com/xbaubes/SistemesOperatius"],
+    ["xarxes","https://github.com/xbaubes/XarxesISeguretat"]
+];
+
 document.getElementsByTagName("footer")[0].innerHTML = `
 <div>
 <img src="https://raw.githubusercontent.com/xbaubes/xbaubes/main/CC.png" alt="CC(BY)">
@@ -12,13 +21,32 @@ document.getElementsByTagName("footer")[0].innerHTML = `
 let navIcons1 = document.getElementsByClassName("icon-container icon-container-modules")[0];
 if (navIcons1 !== undefined) {
     navIcons1.innerHTML = `
-    <a href="index.html"><img src="assets/icons/home.svg" alt="Home"></a>
+    <a href="../index.html"><img src="../assets/icons/home.svg" alt="Home"></a>
     `;
+    let url = window.location.href;
+    for(let ele of modules)
+    {
+        if (url.includes(ele[0])) {
+            navIcons1.innerHTML += `
+            <a href="${ele[1]}" target="_blank" rel="noopener noreferrer"><img src="../assets/icons/github.svg" alt="Github"></a>
+            `;
+        }
+    }
 }
+
 let navIcons2 = document.getElementsByClassName("icon-container icon-container-module-content")[0];
 if (navIcons2 !== undefined) {
     navIcons2.innerHTML = `
-    <a href="index.html"><img src="assets/icons/home.svg" alt="Home"></a>
-    <a href="templateModule.html"><img src="assets/icons/arrow-left.svg" alt="Module"></a>
+    <a href="../index.html"><img src="../assets/icons/home.svg" alt="Home"></a>
+    <a href="../templates/templateModule.html"><img src="../assets/icons/arrow-left.svg" alt="Module"></a>
+    `;
+}
+
+let arrowTop = document.getElementsByClassName("scroll-to-top")[0];
+if (arrowTop !== undefined) {
+    arrowTop.innerHTML = `
+    <a href="#" class="scroll-link">
+        <img src="../assets/icons/arrow-up.svg" alt="Torna amunt" class="scroll-icon">
+    </a>
     `;
 }
