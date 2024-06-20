2261e5709829ea162ba33e57022c4e2cef33809c
Xavier Baubés Parramon <xavierbaubes@gmail.com>
2024-06-20 18:56:31 +0200

    Update components.js

--- components.js
+++ components.js
@@ -1,6 +1,6 @@
 let modules = [
     ["aplicainfo","https://github.com/xbaubes/AplicacionsInformatiques","../../aplicainfo/aplicacions.html"],
-    ["basesdedades","https://github.com/xbaubes/BasesDeDades","../../basesdedades/bdd.html"],
+    ["basesddades","https://github.com/xbaubes/BasesDeDades","../../basesdedades/bdd.html"],
     ["desenvolupaweb","https://github.com/xbaubes/DesenvolupamentWeb","../../desenvolupaweb/web.html"],
     ["programainfo","https://github.com/xbaubes/Programacio","../../programainfo/programacio.html"],
     ["sistemopera","https://github.com/xbaubes/SistemesOperatius","../../sistemopera/so.html"],
