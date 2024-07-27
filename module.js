961f2638a020fe2a50946d7d0613ded9789ef505
Xavier Baubés Parramon <xavierbaubes@gmail.com>
2024-07-27 23:06:19 +0200

    removed comma on final string

--- module.js
+++ module.js
@@ -5,5 +5,5 @@ let modules = [
     ["programainfo","https://github.com/xbaubes/Programacio","../../programainfo/programacio.html"],
     ["sistemopera","https://github.com/xbaubes/SistemesOperatius","../../sistemopera/so.html"],
     ["xarxesinfo","https://github.com/xbaubes/XarxesISeguretat","../../xarxesinfo/xarxes.html"],
-    ["templates","https://github.com/xbaubes","../../templates/templateModule.html"],
+    ["templates","https://github.com/xbaubes","../../templates/templateModule.html"]
 ];
