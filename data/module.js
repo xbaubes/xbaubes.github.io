3454ed1e3d140f5b15276aebf99bd5b33d55ea3a
Xavier Baubés Parramon <xavierbaubes@gmail.com>
2024-07-28 16:26:38 +0200

    renamed to index.html all module main page

--- data/module.js
+++ data/module.js
@@ -1,9 +1,9 @@
 const modules = [
-    ["aplicainfo","https://github.com/xbaubes/AplicacionsInformatiques","../../aplicainfo/apps.html"],
-    ["basesddades","https://github.com/xbaubes/BasesDeDades","../../basesdedades/bdd.html"],
-    ["desenvolupaweb","https://github.com/xbaubes/DesenvolupamentWeb","../../desenvolupaweb/web.html"],
-    ["programainfo","https://github.com/xbaubes/Programacio","../../programainfo/programacio.html"],
-    ["sistemopera","https://github.com/xbaubes/SistemesOperatius","../../sistemopera/so.html"],
-    ["xarxesinfo","https://github.com/xbaubes/XarxesISeguretat","../../xarxesinfo/xarxes.html"],
+    ["aplicainfo","https://github.com/xbaubes/AplicacionsInformatiques","../../aplicainfo/index.html"],
+    ["basesddades","https://github.com/xbaubes/BasesDeDades","../../basesdedades/index.html"],
+    ["desenvolupaweb","https://github.com/xbaubes/DesenvolupamentWeb","../../desenvolupaweb/index.html"],
+    ["programainfo","https://github.com/xbaubes/Programacio","../../programainfo/index.html"],
+    ["sistemopera","https://github.com/xbaubes/SistemesOperatius","../../sistemopera/index.html"],
+    ["xarxesinfo","https://github.com/xbaubes/XarxesISeguretat","../../xarxesinfo/index.html"],
     ["templates","https://github.com/xbaubes","../../templates/templateModule.html"]
 ];
